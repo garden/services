be6e64c379775dcd7ad57bc9393e832f05639944
Jan Keromnes <janx@linux.com>
2012-03-02 23:55:31 +0100

    [Install script] Still working on same error...
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- install.sh
+++ install.sh
@@ -2,27 +2,27 @@
 
 # clone repositories
 
-git clone http://github.com/garden/tree /home/dom/tree
-git clone http://github.com/garden/services /home/dom/services
+git clone http://github.com/garden/tree /home/dom/tree;
+git clone http://github.com/garden/services /home/dom/services;
 
 # generate HTTPS credentials
 
-cd /home/dom/tree && \
-openssl genrsa -aes256 -out https.key 1024 && \
-openssl req -new -key https.key -out https.csr && \
-openssl x509 -req -days 365 -in https.csr -signkey https.key -out https.crt && \
-openssl rsa -in https.key -out https.key.new && \
-mv https.key{.new,}
+cd /home/dom/tree
+openssl genrsa -aes256 -out https.key 1024;
+openssl req -new -key https.key -out https.csr;
+openssl x509 -req -days 365 -in https.csr -signkey https.key -out https.crt;
+openssl rsa -in https.key -out https.key.new;
+mv https.key{.new,};
 
 # install service scripts
 
-sudo cp /home/dom/services/tree /etc/init.d/
-sudo cp /home/dom/services/update /etc/init.d/
-sudo cp /home/dom/services/warden /etc/init.d/
-sudo cp /home/dom/services/redirect /etc/init.d/
+sudo cp /home/dom/services/tree /etc/init.d/;
+sudo cp /home/dom/services/update /etc/init.d/;
+sudo cp /home/dom/services/warden /etc/init.d/;
+sudo cp /home/dom/services/redirect /etc/init.d/;
 
 # start all services
 
-sudo service redirect start
-sudo service warden start
-#sudo service update start
+sudo service redirect start;
+sudo service warden start;
+#sudo service update start;
