648757c39521e68c1495c01970ab3ab583a87282
Jan Keromnes <janx@linux.com>
2012-03-02 23:41:09 +0100

    [OpenSSL] Test
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- install.sh
+++ install.sh
@@ -5,21 +5,21 @@
 git clone http://github.com/garden/tree /home/dom/tree
 git clone http://github.com/garden/services /home/dom/services
 
+# generate HTTPS credentials
+
+cd /home/dom/tree && \
+openssl genrsa -aes256 -out https.key.tmp 1024 && \
+openssl req -new -key https.key.tmp -out https.csr && \
+openssl rsa -in https.key.tmp -out https.key && \
+openssl x509 -req -days 365 -in https.csr -signkey https.key -out https.crt && \
+rm https.key.tmp
+
 # install service scripts
 
 sudo cp /home/dom/services/tree /etc/init.d/
 sudo cp /home/dom/services/update /etc/init.d/
 sudo cp /home/dom/services/warden /etc/init.d/
 sudo cp /home/dom/services/redirect /etc/init.d/
-
-# generate HTTPS credentials
-
-cd /home/dom/tree
-openssl genrsa -aes256 -out https.key.tmp 1024
-openssl req -new -key https.key.tmp -out https.csr
-openssl rsa -in https.key.tmp -out https.key
-openssl x509 -req -days 365 -in https.csr -signkey https.key -out https.crt
-rm https.key.tmp
 
 # start all services
 
