fa0dc154f02c389ed614a1b30544eecdf3935f2c
Jan Keromnes <janx@linux.com>
2012-03-03 00:20:04 +0100

    Nodes...
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ git clone http://github.com/garden/services /home/dom/services;
 
 cd /home/dom/tree;
 openssl genrsa -aes256 -out https.key 1024;
-openssl req -new -key https.key -config /home/dom/services/https.conf -out https.csr;
+openssl req -new -nodes -key https.key -config /home/dom/services/https.conf -out https.csr;
 openssl x509 -req -days 365 -in https.csr -signkey https.key -out https.crt;
 cp https.key{,.orig};
 openssl rsa -in https.key.orig -out /home/dom/tree/https.key;
