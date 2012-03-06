d5475fe7f0a1b893fb6429ed461685f0956ef5ac
Jan Keromnes <janx@linux.com>
2012-03-06 12:18:19 +0100

    [OpenSSL] SSL cert generation bugfix
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ git clone http://github.com/garden/services /home/dom/services;
 
 # generate HTTPS credentials
 
-cd /home/dom/tree;
-openssl genrsa -aes256 -out https.key 1024;
-openssl req -new -nodes -key https.key -config /home/dom/services/https.conf -out https.csr;
-openssl x509 -req -days 365 -in https.csr -signkey https.key -out https.crt;
-cp https.key{,.orig};
-openssl rsa -in https.key.orig -out /home/dom/tree/https.key;
+cd /home/dom/tree
+openssl genrsa -aes256 -out https.key 1024
+openssl req -new -nodes -key https.key -out https.csr
+openssl x509 -req -days 365 -in https.csr -signkey https.key -out https.crt
+cp https.key{,.orig}
+openssl rsa -in https.key.orig -out https.key
 
 # install service scripts
 
