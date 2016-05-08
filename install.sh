71f61247bd8f1f48a824d24cdb01adc5e35e2fae
Thaddee Tyl <thaddee.tyl@gmail.com>
2016-05-08 14:34:13 +0200

    Convert to systemd

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ if ! which git > /dev/null;
   then echo "Git is not installed. Installation of git in progress.";
   case $PM in
     ubuntu)
-      sudo apt-get install git-core;;
+      sudo apt install git-core;;
     nix)
       sudo nix-env -i git;;
   esac
@@ -23,7 +23,7 @@ if ! which node > /dev/null;
   then echo "Node is not installed. Installation of Node in progress.";
   case $PM in
     ubuntu)
-      sudo apt-get install nodejs;;
+      sudo apt install nodejs;;
     nix)
       sudo nix-env -i nodejs;;
   esac
@@ -34,7 +34,7 @@ if ! which openssl > /dev/null;
   then echo "openssl is not installed. Installation of openssl in progress.";
   case $PM in
     ubuntu)
-      sudo apt-get install openssl;;
+      sudo apt install openssl;;
     nix)
       sudo nix-env -i openssl;;
   esac
@@ -43,27 +43,38 @@ fi;
 
 # clone repositories
 
-git clone http://github.com/garden/tree /home/dom/tree
-git clone http://github.com/garden/services /home/dom/services
+git clone https://github.com/garden/services /home/dom/services
+git clone https://github.com/garden/tree /home/dom/tree
 
-# generate HTTPS credentials
+pushd /home/dom/tree
 
-cd /home/dom/tree
-openssl genrsa -aes256 -out https.key 1024
-openssl req -new -nodes -key https.key -out https.csr
-openssl x509 -req -days 365 -in https.csr -signkey https.key -out https.crt
-cp https.key{,.orig}
-openssl rsa -in https.key.orig -out https.key
+  # populate tree
+
+  git clone https://github.com/garden/plugs.git
+  make load
+  npm install
+
+  # generate HTTPS credentials
+
+  cd /home/dom/tree
+  openssl genrsa -aes256 -out https.key 1024
+  openssl req -new -nodes -key https.key -out https.csr
+  openssl x509 -req -days 365 -in https.csr -signkey https.key -out https.crt
+  cp https.key{,.orig}
+  openssl rsa -in https.key.orig -out https.key
+
+popd
 
 # install service scripts
 
-sudo cp /home/dom/services/tree /etc/init.d/
-sudo cp /home/dom/services/update /etc/init.d/
-sudo cp /home/dom/services/warden /etc/init.d/
-sudo cp /home/dom/services/redirect /etc/init.d/
+sudo cp /home/dom/services/tree.service /etc/systemd/system/
+sudo cp /home/dom/services/redirect.service /etc/systemd/system/
+sudo cp /home/dom/services/update.service /etc/systemd/system/
+
+systemctl daemon-reload
 
 # start all services
 
-sudo service redirect start
-sudo service warden start
-sudo service update start
+systemctl start tree.service
+systemctl start redirect.service
+systemctl start update.service
--- updator.sh
+++ updator.sh
@@ -3,4 +3,4 @@
 cd /home/dom/tree >> /home/dom/services/update.log 2>&1
 make backup >> /home/dom/services/update.log 2>&1
 git pull origin master >> /home/dom/services/update.log 2>&1
-sudo service tree stop >> /home/dom/services/update.log 2>&1
+systemctl stop tree.service >> /home/dom/services/update.log 2>&1
