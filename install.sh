e740eaca594ab182148e533c92bca0c9306724bf
Thaddee Tyl <thaddee.tyl@gmail.com>
2016-05-08 14:38:07 +0200

    Omit needless cd

--- install.sh
+++ install.sh
@@ -56,7 +56,6 @@ pushd /home/dom/tree
 
   # generate HTTPS credentials
 
-  cd /home/dom/tree
   openssl genrsa -aes256 -out https.key 1024
   openssl req -new -nodes -key https.key -out https.csr
   openssl x509 -req -days 365 -in https.csr -signkey https.key -out https.crt
